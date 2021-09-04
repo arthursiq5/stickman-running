f4ac3dd1217d1daf3a33c5c705efe3ae9b026614
arthursiq5 <arthur.siq5@gmail.com>
2021-09-04 16:24:25 -0300

    Criada estrutura básica do jogo

---
+++ main.lua
@@ -0,0 +1,17 @@
+local anim = require 'anim8/anim8'
+
+local imagem, animation
+local posX = 100
+local direcao = true
+
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
